e44cc4f27c8658bee90d39db1bb534bf4084b120
JohnMitysumy <johnmitysumy@gmail.com>
2024-01-17 16:58:09 -0300

    Ajuste de css

--- menu.php
+++ menu.php
@@ -1,9 +1,9 @@
 <nav class="menu">
         <ul>
             <li>
-                <a href="/" style="color:white; font-size:20px;">Inicio</a>
-                <a href="/cadastrar.php" style="color:white; font-size:20px; margin-left: 20px;">Cadastrar</a>
-                <a href="/edit_list.php" style="color:white; font-size:20px; margin-left: 20px;">Editar</a>
+                <a href="/" style="color:white; font-size:20px; "><button style="background-color: #333333d3;" class="btn btn-success">INICIO</button></a>
+                <a href="/cadastrar.php" style="color:white; font-size:20px; margin-left: 20px;"><button style="background-color: #333333d3;" class="btn btn-success">CADASTRAR</button></a>
+                <a href="/edit_list.php" style="color:white; font-size:20px; margin-left: 20px;"><button style="background-color: #333333d3;" class="btn btn-success">EDITAR</button></a>
             </li>
         </ul>
     </nav>
